f37121d2d7353e60de21b7387c95fdfd147c4c91
miachen67 <miachen2005@gmail.com>
2024-04-06 16:08:42 -0700

    added image to locationCard

--- client/src/app/components/LocationCard.js
+++ client/src/app/components/LocationCard.js
@@ -1,8 +1,13 @@
+import Image from 'next/image'
+
 const LocationCard = ({image, locationName, rating, features}) => {
     return (
         <div className="locationCard">
             <div className="locationImage">
-                {/*link image*/}
+                <Image
+                src = {image}
+                alt = "Image of " + {locationName}
+                />
                 <h5>{locationName}</h5>
                 <h6>{rating}</h6>
                 <p>{features}</p>
